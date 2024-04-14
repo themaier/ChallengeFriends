eb527b5e6381d2e845a9fccf7e52694f6aac2e4a
Tobias <tobias.maier@st.oth-regensburg.de>
2024-04-14 23:15:37 +0200

    remove https agen

--- frontend/src/services/challenge.service.js
+++ frontend/src/services/challenge.service.js
@@ -1,63 +1,60 @@
 import axios from 'axios';
-import https from 'https'
 
 const ipv4 = import.meta.env.VITE_IPV4 || 'http://localhost:8000';
 const API_URL = `${ipv4}/challenges/`;
-const httpsAgent = new https.Agent({ rejectUnauthorized: false });
-const config = API_URL.startsWith('https://') ? { httpsAgent } : {};
 
 class ChallengeService {
 
     async createChallenge(challenge) {
-        return await axios.post(API_URL, challenge, config).then(response => {
+        return await axios.post(API_URL, challenge).then(response => {
             return response
         })
     }
 
     async getPendingChallenges(id) {
-        return await axios.get(API_URL + id + '/pending', config).then(response => {
+        return await axios.get(API_URL + id + '/pending').then(response => {
             return response
         })
     }
 
     async getAcceptedChallenges(id) {
-        return await axios.get(API_URL + id + '/accepted', config).then(response => {
+        return await axios.get(API_URL + id + '/accepted').then(response => {
             return response
         })
     }
     
     async getCreatedChallenges(id) {
-        return await axios.get(API_URL + id + '/created', config).then(response => {
+        return await axios.get(API_URL + id + '/created').then(response => {
             return response
         })
     }
 
     async acceptChallenge(id) {
-        return await axios.put(API_URL + id + '/accept', config).then(response => {
+        return await axios.put(API_URL + id + '/accept').then(response => {
             return response
         })
     }
 
     async declineChallenge(id) {
-        return await axios.put(API_URL + id + '/decline', config).then(response => {
+        return await axios.put(API_URL + id + '/decline').then(response => {
             return response
         })
     }
 
     async getCompletedChallengesByUser(id, userId) {
-        return await axios.get(API_URL + id + '/done?logged_in_user_id=' + userId, config).then(response => {
+        return await axios.get(API_URL + id + '/done?logged_in_user_id=' + userId).then(response => {
             return response
         })
     }
 
     async getCompletedChallengesByTag(hashtag, id) {
-        return await axios.get(API_URL + hashtag + '?userId=' + id, config).then(response => {
+        return await axios.get(API_URL + hashtag + '?userId=' + id).then(response => {
             return response
         })
     }
 
     async getTrendingChallenges(id) {
-        return await axios.get(API_URL + 'latest/' + 10 + '?userId=' + id, config).then(response => {
+        return await axios.get(API_URL + 'latest/' + 10 + '?userId=' + id).then(response => {
             return response
         })
     }
@@ -65,8 +62,7 @@ class ChallengeService {
     async likeChallenge(challengeId, userId) {
         return await axios.put(API_URL + challengeId + '/like', {
             user_id: userId,
-            challenge_id: challengeId,
-            config
+            challenge_id: challengeId
         }).then(response => {
             return response
         })
@@ -79,8 +75,7 @@ class ChallengeService {
         return await axios.put(`${API_URL}${challengeId}/done`, formData, {
             headers: {
             'Content-Type': 'multipart/form-data',
-            },
-            config
+            }
         }).then(response => {
             return response
         })
@@ -98,8 +93,7 @@ class ChallengeService {
         return await axios.put(`${API_URL}${challengeId}/comment`, formData, {
             headers: {
             'Content-Type': 'multipart/form-data',
-            },
-            config
+            }
         }).then(response => {
             return response
         })
--- frontend/src/services/friendship.service.js
+++ frontend/src/services/friendship.service.js
@@ -2,34 +2,32 @@ import axios from 'axios'
 
 const ipv4 = import.meta.env.VITE_IPV4 || 'http://localhost:8000';
 const API_URL = `${ipv4}/friendship/`
-const config = API_URL.startsWith('https://') ? { httpsAgent } : {};
 
 class FriendshipService {
 
     async addFriend(id, friendId) {
         return await axios.post(API_URL, {
             user_id: id,
-            friend_user_id: friendId,
-            config
+            friend_user_id: friendId
         }).then(response => {
             return response
         })
     }
 
     async getFriends() {
-        return await axios.get(API_URL, config).then(response => {
+        return await axios.get(API_URL).then(response => {
             return response
         })
     }
 
     async getFriend(id) {
-        return await axios.get(API_URL + id, config).then(response => {
+        return await axios.get(API_URL + id).then(response => {
             return response
         })
     }
 
     async deleteFriend(user_id, friend_user_id) {
-        return await axios.delete(API_URL + "?user_id=" + user_id + "&friend_user_id=" + friend_user_id, config).then(response => {
+        return await axios.delete(API_URL + "?user_id=" + user_id + "&friend_user_id=" + friend_user_id).then(response => {
             return response
         })
     }
